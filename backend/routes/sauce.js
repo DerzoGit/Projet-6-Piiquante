60c24d478f755a29e0d2e1c916a64f2f95744e64
DerzoGit <journetalexandre.contact@gmail.com>
2021-10-25 22:17:45 +0200

    comment routes/sauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,14 +5,17 @@ const router = express.Router();
 // Import du controller de sauce
 const sauceCtrl = require("../controllers/sauce");
 
+// Route pour créer une sauce
 router.post("/", sauceCtrl.createSauce);
-
+// Route pour modifier une sauce
 router.put("/:id", sauceCtrl.modifySauce);
-
+// Route pour supprimer une sauce
 router.delete("/:id", sauceCtrl.deleteSauce);
-
+// Route pour récupérer une sauce
 router.get("/:id", sauceCtrl.getOneSauce);
-
+// Route pour récupérer toutes les sauces
 router.get("/", sauceCtrl.getAllSauce);
+// Route pour récupérer les likes des sauces
+// router.post("/:id/like", sauceCtrl.likeSauce);
 
 module.exports = router;
