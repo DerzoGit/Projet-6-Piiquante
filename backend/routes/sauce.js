df8adf61c6db62617384aeb232ebabc88c3b1cc2
DerzoGit <journetalexandre.contact@gmail.com>
2021-10-25 21:23:02 +0200

    add route/sauce.js

---
+++ backend/routes/sauce.js
@@ -0,0 +1,9 @@
+const express = require("express");
+// Création d'un router avec la méthode d'express
+const router = express.Router();
+
+// Import du model de sauce
+const Sauce = require("../models/sauce");
+
+
+module.exports = router;
