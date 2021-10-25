316c5e976bc4162f9a96138eff7f3d4da45493f9
DerzoGit <journetalexandre.contact@gmail.com>
2021-10-25 22:04:39 +0200

    add routes/sauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,8 +2,17 @@ const express = require("express");
 // Création d'un router avec la méthode d'express
 const router = express.Router();
 
-// Import du model de sauce
-const Sauce = require("../models/sauce");
+// Import du controller de sauce
+const sauceCtrl = require("../controllers/sauce");
 
+router.post("/", sauceCtrl.createSauce);
+
+router.put("/:id", sauceCtrl.modifySauce);
+
+router.delete("/:id", sauceCtrl.deleteSauce);
+
+router.get("/:id", sauceCtrl.getOneSauce);
+
+router.get("/", sauceCtrl.getAllSauce);
 
 module.exports = router;
